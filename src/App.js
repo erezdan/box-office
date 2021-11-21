40bca9b064a046ff97c7457daf6ca2b54c8a0069
erezdan <erezdanmail@gmail.com>
2021-11-21 16:05:32 +0200

    add switch and router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 /* eslint-disable func-names */
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 const App = function () {
-  return <div>worked inv.</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred page
+      </Route>
+
+      <Route>Not found page 404</Route>
+    </Switch>
+  );
 };
 
 export default App;
