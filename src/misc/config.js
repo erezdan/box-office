09eed8049f9bed0c87d5c8a4c97f464cc6e8d6ce
erezdan <erezdanmail@gmail.com>
2021-11-22 15:38:18 +0200

    displayed results

---
+++ src/misc/config.js
@@ -0,0 +1,10 @@
+import { findByDisplayValue } from '@testing-library/react';
+
+const API_BASE_URL = 'https://api.tvmaze.com';
+
+export async function apiGet(queryString) {
+  const responce = await fetch(`${API_BASE_URL}${queryString}`).then(r =>
+    r.json()
+  );
+  return responce;
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
+
+// tvmaze.com
 
 const Home = () => {
-  return <MainPageLayout>This is Home page</MainPageLayout>;
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+
+  const onInputChange = en => {
+    setInput(en.target.value);
+  };
+
+  const search = () => {
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
+
+  const onButtonClick = () => {
+    search(input);
+  };
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) search(input);
+  };
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => {
+            return <div key={item.show.id}>{item.show.name}</div>;
+          })}
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        val={input}
+      />
+      <button type="button" onClick={onButtonClick}>
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
 };
 
 export default Home;
