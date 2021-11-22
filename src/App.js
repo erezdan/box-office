feb5ce468fef1e2a22fa8d0e27218eb17716cd47
erezdan <erezdanmail@gmail.com>
2021-11-22 12:10:52 +0200

    added layout management and create title

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 /* eslint-disable func-names */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from './components/Navs';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 const App = () => {
   return (
-    <div>
-      <Navs />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
 
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-
-        <Route>
-          <div>Not found</div>
-        </Route>
-      </Switch>
-    </div>
+      <Route>
+        <div>Not found</div>
+      </Route>
+    </Switch>
   );
 };
 
